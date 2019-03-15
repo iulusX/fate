b88ca1f667f01fd3a8f62fc6968bf4a09d305eee
iulus <1946265662@qq.com>
2019-03-15 20:14:49 +0800

    初始化提交

---
+++ src/main/java/com/iulus/JLTest/JlTestApplication.java
@@ -0,0 +1,13 @@
+package com.iulus.JLTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlTestApplication.class, args);
+	}
+
+}
